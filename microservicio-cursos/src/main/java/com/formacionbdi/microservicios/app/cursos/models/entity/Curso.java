9a98d6c898352e36aa4fdaa12f85a6723e40754f
fran89rbb <fran89rbb@gmail.com>
2022-08-17 12:57:59 +0200

    Modificando relación en entity Curso con alumnos

--- microservicio-cursos/src/main/java/com/formacionbdi/microservicios/app/cursos/models/entity/Curso.java
+++ microservicio-cursos/src/main/java/com/formacionbdi/microservicios/app/cursos/models/entity/Curso.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -16,8 +17,10 @@ import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 import javax.validation.constraints.NotEmpty;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.formacionbdi.microservicios.app.commons.alumnos.models.entity.Alumno;
 import com.formacionbdi.microservicios.app.commons.examenes.models.entity.Examen;
 
@@ -28,27 +31,34 @@ public class Curso {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@NotEmpty
 	private String nombre;
 
 	@Column(name = "create_at")
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date createAt;
-	
-	@OneToMany(fetch = FetchType.LAZY)
+
+	@JsonIgnoreProperties(value = { "curso" }, allowSetters = true)
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "curso", cascade = CascadeType.ALL)
+	private List<CursoAlumno> cursoAlumnos;
+
+	// @OneToMany(fetch = FetchType.LAZY)
+	@Transient
 	private List<Alumno> alumnos;
-	
+
 	@ManyToMany(fetch = FetchType.LAZY)
 	private List<Examen> examenes;
-	
+
 	@PrePersist
 	public void prePersist() {
 		this.createAt = new Date();
 	}
-	
+
 	public Curso() {
-		this.alumnos = new ArrayList<>();		this.examenes = new ArrayList<>();
+		this.alumnos = new ArrayList<>();
+		this.examenes = new ArrayList<>();
+		this.cursoAlumnos = new ArrayList<>();
 	}
 
 	public Long getId() {
@@ -82,11 +92,19 @@ public class Curso {
 	public void setAlumno(List<Alumno> alumnos) {
 		this.alumnos = alumnos;
 	}
-	
+
+	public List<CursoAlumno> getCursoAlumnos() {
+		return cursoAlumnos;
+	}
+
+	public void setCursoAlumnos(List<CursoAlumno> cursoAlumnos) {
+		this.cursoAlumnos = cursoAlumnos;
+	}
+
 	public void addAlumno(Alumno alumno) {
 		this.alumnos.add(alumno);
 	}
-	
+
 	public void removeAlumno(Alumno alumno) {
 		this.alumnos.remove(alumno);
 	}
@@ -98,13 +116,21 @@ public class Curso {
 	public void setExamenes(List<Examen> examenes) {
 		this.examenes = examenes;
 	}
-	
+
 	public void addExamen(Examen examen) {
 		this.examenes.add(examen);
 	}
-	
+
 	public void removeExamen(Examen examen) {
 		this.examenes.remove(examen);
+	}
+	
+	public void addCursoAlumnos(CursoAlumno cursoAlumno) {
+		this.cursoAlumnos.add(cursoAlumno);
+	}
+	
+	public void removeCursoAlumnos(CursoAlumno cursoAlumno) {
+		this.cursoAlumnos.remove(cursoAlumno);
 	}
 
 }
